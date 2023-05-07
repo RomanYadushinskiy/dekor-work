eb8b1001ed7ae0dcc366417b9ad6631d72fcfa06
rdush <rdush824@gmail.com>
2023-05-07 23:54:25 +0300

    add our works

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+const tabsItem = document.querySelectorAll('.tabs__btn-item');
+const tabsContent = document.querySelectorAll('.tabs__content-item');
+
+tabsItem.forEach(function (element) {
+  element.addEventListener('click', open);
+});
+
+function open(evt) {
+  const tabsTarget = evt.currentTarget;
+  const button = tabsTarget.dataset.button;
+
+  tabsItem.forEach(function (item) {
+    item.classList.remove('tabs__btn-item--active');
+  });
+
+  tabsTarget.classList.add('tabs__btn-item--active');
+
+  tabsContent.forEach(function (item) {
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  document.querySelector(`#&{button}`).classList.add('tabs__content-item--active');
+};
+
 const swiper = new Swiper(".swiper", {
   effect: "fade",
   pagination: {
